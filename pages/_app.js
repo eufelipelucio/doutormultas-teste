dc41e4101698ff47df10e42ce562fc0fb155e9e7
Felipe Lucio <felipesou94@gmail.com>
2022-03-22 01:57:43 -0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import '../styles/header.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ pages/components/header.js
@@ -0,0 +1,20 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react'
+import Image from 'next/image'
+import Logo from '../../public/logo.webp'
+import Doutor from '../../public/doutor.jpg'
+export default function Header() {
+  return (
+    <>
+        <div className='logo-header' >
+              <Image
+              src={Logo} 
+              width={300} 
+              height={75}/>
+        </div>
+        <div className='logo-header'>
+          <Image src={Doutor} />
+        </div>
+    </>
+  )
+}
